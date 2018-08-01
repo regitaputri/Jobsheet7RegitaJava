67c13e0067fc8eeffdfa3611335bc3fa4a83dc39
ASUS <ASUS@LAPTOP-HV0JURP4>
2018-08-01 09:27:39 +0700

    Initial Commit

---
+++ src/jobsheet7/Latihan1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+        
+        String identitas = "regita putri rozi / x rpl 5 / 35";
+        System.out.println("Identitas : "+ identitas);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. l");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5){
+            System.out.println("isi variabel a : "+a);
+            System.out.println("nilai a lebih dari 5");
+        }
+    }
+}
