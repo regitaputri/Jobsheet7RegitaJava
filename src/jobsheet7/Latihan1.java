9238d13f564bb050779c9880b58c5815087ad1c5
ASUS <ASUS@LAPTOP-HV0JURP4>
2018-08-01 09:42:32 +0700

    Coba IF.

--- src/jobsheet7/Latihan1.java
+++ src/jobsheet7/Latihan1.java
@@ -32,6 +32,13 @@ public class Latihan1 {
         if(a > 5){
             System.out.println("isi variabel a : "+a);
             System.out.println("nilai a lebih dari 5");
+            
+            System.out.println("\nIF..ELSE..");
+            if(a > 5) {
+                System.out.println("isi variabel a : "+ a);
+                System.out.println("nilai a kurang dari 5");
+            } else System.out.println("nilai a lebih dari atau sama dengan 5");
+            
+        }
         }
     }
-}
