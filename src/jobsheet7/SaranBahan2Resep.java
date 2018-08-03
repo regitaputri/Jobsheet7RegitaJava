8930761811909ccd59c79820dc8aebd5fc47b290
ASUS <ASUS@LAPTOP-HV0JURP4>
2018-08-03 11:29:27 +0700

    Initial Commit

---
+++ src/jobsheet7/SaranBahan2Resep.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "regita putri rozi / x rpl 5 / 35";
+        System.out.println("identitas :" + identitas);
+        
+        System.out.println("\nSaran Resep Dari Bahan Milik Anda\n");
+        
+        
+    }
+}
